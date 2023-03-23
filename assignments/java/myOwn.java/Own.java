c16c92d8a779bd81b1a1b236f8914eab3d9f475e
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-03-23 12:29:07 -0500

    finished first java hw

---
+++ assignments/java/myOwn.java/Own.java
@@ -0,0 +1,7 @@
+public class Own{
+  public static void main(String [] args){
+    System.out.println("My name is Emily");
+    System.out.println("I am 22 years old");
+    System.out.println("My hometown is Dallas, TX");
+  }
+}
